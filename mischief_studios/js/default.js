b3a86d87a309a4b18b216d9907e11671369a6f01
Natasha Gaffer <ngaffer@gmail.com>
2017-04-05 23:52:01 -0500

    moved .git to new_theme directory

--- mischief_studios/js/default.js
+++
@@ -1,9 +0,0 @@
-window.onscroll = function() {myFunction()};
-
-function myFunction() {
-    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-        document.getElementById("header-bar").className += " scrolled";
-    } else {
-        document.getElementById("header-bar").classList.remove("scrolled");
-    }
-}
