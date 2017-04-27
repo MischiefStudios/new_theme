59e521fde722df36bda4c3017c5c56aa5f75ffd7
Natasha Gaffer <ngaffer@gmail.com>
2017-04-27 13:31:19 -0500

    updated column widht in header-nav

--- elements/header.php
+++ elements/header.php
@@ -5,13 +5,13 @@ $this->inc('elements/header_top.php'); ?>
 <header class="header-bar">
 	<div class="container">
 		<div class="row">
-			<div class="header-brand col-med-4 col-sm-5 col-xs-10">
+			<div class="header-brand col-md-3 col-sm-5 col-xs-10">
 				<?php
 				$a = new GlobalArea('Header Site Title');
 				$a->display();
 				?>
 			</div>
-			<div class="header-nav col-md-5 col-sm-7 col-xs-2">
+			<div class="header-nav col-md-9 col-sm-7 col-xs-2">
 				<?php
 				$a = new GlobalArea('Header Navigation');
 				$a->display();
